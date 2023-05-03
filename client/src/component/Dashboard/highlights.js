da08e901370f7ce19195d6e1e58de6e57448d58d
Minyan910 <minyan.wang@colorado.edu>
2023-05-03 10:14:37 -0600

    highlights shows 8 random recipes

--- client/src/component/Dashboard/highlights.js
+++ client/src/component/Dashboard/highlights.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-import randomRecipes from './randomRecipes.json';
+// import randomRecipes from './randomRecipes.json';
 
-
+const API_URL = "https://www.themealdb.com/api/json/v2/9973533/randomselection.php";
 
 function Highlights( { route, navigation}) {
     const { state } = useLocation();
+
+    const [recipes, setRecipes] = useState([]);
+    // const [searchTerm, setSearchTerm] = useState([]);
+
+    const searchRecipes = async () => {
+        const response = await fetch(`${API_URL}`);
+        const data1 = await response.json();
+        console.log(data1);
+        console.log(typeof(data1));
+        console.log(data1.meals);
+        console.log(typeof(data1.meals));
+        setRecipes(data1.meals);
+        console.log("clicked");
+        console.log(recipes.length);
+    }
+
+    useEffect(() => {
+        searchRecipes();
+    }, []);
+
     return (
         <div class="dashboard">
             <nav class="navbar inverse">
@@ -33,26 +53,26 @@ function Highlights( { route, navigation}) {
             </div>
 
             <div class="container">
-                {randomRecipes.slice(0,4).map(function (data, index) {
-                if (index < 4) {
-                    return (
-                        <div class="card item" style={{ width: 220}}>
-                            <img src={data.strMealThumb} alt="" />
-                            <div class="card-body">
-                                <h5 class="card-title">{data.strMeal}</h5>
-                                <p class='card-text'>
-                                {data.strArea}<br />
-                                Ingredient: {data.strCategory}
-                                </p>
-                                <a href="/" class="btn btn-primary">Add to list</a>
-                                <i class="bi bi-bookmark-heart float-end"></i>
-                                {/* <i class="bi bi-bookmark-heart-fill float-end"></i> */}
+                {recipes.slice(0,8).map(function (data, index) {
+                    if (index < 8) {
+                        return (
+                            <div class="card item" style={{ width: 220}}>
+                                <img src={data.strMealThumb} alt="" />
+                                <div class="card-body">
+                                    <h5 class="card-title">{data.strMeal}</h5>
+                                    <p class='card-text'>
+                                    {data.strArea}<br />
+                                    Ingredient: {data.strCategory}
+                                    </p>
+                                    <a href="/" class="btn btn-primary">Add to list</a>
+                                    <i class="bi bi-bookmark-heart float-end"></i>
+                                    {/* <i class="bi bi-bookmark-heart-fill float-end"></i> */}
+                                </div>
                             </div>
-                        </div>
-                    )
-                }
-                return 0;
-            })}
+                        )
+                    }
+                    return 0;
+                })}
             </div>
 
 
